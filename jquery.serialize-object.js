ff662a5f35767d29410f8ed6284075ed9262e089
Paul Macek <paulmacek@gmail.com>
2014-03-10 21:13:58 -0500

    Rebuild using new build process

--- jquery.serialize-object.js
+++ jquery.serialize-object.js
@@ -1,4 +1,11 @@
-;(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/**
+ * jQuery serializeObject
+ * @copyright 2014, macek <paulmacek@gmail.com>
+ * @link https://github.com/macek/jquery-serialize-object
+ * @license BSD
+ * @version 2.0.1
+ */
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var FormSerializer = module.exports = function FormSerializer(helper) {
   this._helper    = helper;
   this._object    = {};
@@ -121,4 +128,3 @@ var FormSerializer = require("./form-serializer"),
 })(jQuery);
 
 },{"./form-serializer":1,"./helper":2}]},{},[3])
-;
