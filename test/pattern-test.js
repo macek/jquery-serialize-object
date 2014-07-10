3c711e7b1bab0ae6cad32704fee19dea4fd9f391
Paul Macek <paulmacek@gmail.com>
2014-07-10 16:49:30 -0500

    move pattern tests to a separate file

---
+++ test/pattern-test.js
@@ -0,0 +1,46 @@
+var assert = chai.assert;
+
+describe("patterns", function() {
+
+  var original = $.extend({}, FormSerializer.patterns);
+
+  function resetPatterns() {
+    $.extend(FormSerializer.patterns, original);
+  }
+
+  it("exposes FormSerializer.patterns", function() {
+    var p = FormSerializer.patterns;
+    assert.typeOf(p.validate, "regexp");
+    assert.typeOf(p.key,      "regexp");
+    assert.typeOf(p.push,     "regexp");
+    assert.typeOf(p.fixed,    "regexp");
+    assert.typeOf(p.named,    "regexp");
+  });
+
+  it("override pattern to allow dot notation", function() {
+    $.extend(FormSerializer.patterns, {
+      validate: /^[a-z][a-z0-9_]*(?:\.[a-z0-9_]+)*(?:\[\])?$/i
+    });
+
+    var f = new FormSerializer($);
+    f.addPair({name: "a.b.c", value: "d"});
+    assert.strictEqual(f.serializeJSON(), '{"a":{"b":{"c":"d"}}}');
+
+    resetPatterns();
+  });
+
+  it("override patterns to allow hyphens", function() {
+    $.extend(FormSerializer.patterns, {
+      validate: /^[a-z][a-z0-9_-]*(?:\[(?:\d*|[a-z0-9_]+)\])*$/i,
+      key:      /[a-z0-9_-]+|(?=\[\])/gi,
+      named:    /^[a-z0-9_-]+$/i
+    });
+
+    var f = new FormSerializer($);
+    f.addPair({name: "a-b[c]", value: "d"});
+    assert.strictEqual(f.serializeJSON(), '{"a-b":{"c":"d"}}');
+
+    resetPatterns();
+  });
+
+});
--- test/plugin-test.js
+++ test/plugin-test.js
@@ -12,39 +12,4 @@ describe("jQuery plugin", function() {
     assert.strictEqual($.fn.serializeJSON, FormSerializer.serializeJSON);
   });
 
-  it("exposes FormSerializer.patterns", function() {
-    var p = FormSerializer.patterns;
-    assert.typeOf(p.validate, "regexp");
-    assert.typeOf(p.key,      "regexp");
-    assert.typeOf(p.push,     "regexp");
-    assert.typeOf(p.fixed,    "regexp");
-    assert.typeOf(p.named,    "regexp");
-  });
-
-  it("Override pattern using dot notation", function() {
-    var form = $('<form><input name="a.b.c" value="d" />');
-
-    assert.deepEqual(form.serializeObject(), {});
-
-    $.extend(FormSerializer.patterns, {
-      validate: /^[a-z][a-z0-9_]*(?:\.[a-z0-9_]+)*(?:\[\])?$/i
-    });
-
-    assert.deepEqual(form.serializeObject(), { a: { b: { c: 'd' } } });
-  });
-
-  it("Override patterns allowing hyphens", function() {
-    var form = $('<form><input name="a-b[c]" value="d" />');
-
-    assert.deepEqual(form.serializeObject(), {});
-
-    $.extend(FormSerializer.patterns, {
-      validate: /^[a-z][a-z0-9_-]*(?:\[(?:\d*|[a-z0-9_]+)\])*$/i,
-      key:      /[a-z0-9_-]+|(?=\[\])/gi,
-      named:    /^[a-z0-9_-]+$/i
-    });
-
-    assert.deepEqual(form.serializeObject(), { 'a-b': { c: 'd' } });
-  });
-
 });
