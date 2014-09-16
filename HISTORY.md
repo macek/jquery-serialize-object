4d1738743253f0cda34973e73b9895d37446657d
Paul Macek <paulmacek@gmail.com>
2014-09-16 14:50:31 -0500

    bump version to 2.4.2

--- HISTORY.md
+++ HISTORY.md
@@ -1,3 +1,7 @@
+2.4.2
+-----
+* add missing ignore entry to bower.json. wups!
+
 2.4.1
 -----
 * change deprecated component.json to bower.json
--- jquery.serialize-object.js
+++ jquery.serialize-object.js
@@ -3,7 +3,7 @@
  * @copyright 2014, macek <paulmacek@gmail.com>
  * @link https://github.com/macek/jquery-serialize-object
  * @license BSD
- * @version 2.4.1
+ * @version 2.4.2
  */
 (function(root, factory) {
 
