444c161d639943797d8cd8da68533bff3765eeec
Paul Macek <paulmacek@gmail.com>
2014-09-16 02:47:04 -0500

    bump version to 2.3.3

--- jquery.serialize-object.js
+++ jquery.serialize-object.js
@@ -3,7 +3,7 @@
  * @copyright 2014, macek <paulmacek@gmail.com>
  * @link https://github.com/macek/jquery-serialize-object
  * @license BSD
- * @version 2.3.2
+ * @version 2.3.3
  */
 (function(root, factory) {
 
